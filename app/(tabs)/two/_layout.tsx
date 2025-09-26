7cb403a40a0637c0ff4dc73b3f784dd8b0e4add4
stvshy <matisp6@op.pl>
2025-09-26 23:59:33 +0200

    refactor: enhance TwoStack layout by integrating theme support for background color and updating screen options for improved visual consistency and user experience

--- app/(tabs)/two/_layout.tsx
+++ app/(tabs)/two/_layout.tsx
@@ -1,11 +1,31 @@
 import { Stack } from "expo-router";
+import { useTheme } from "react-native-paper";
 
 export default function TwoStack() {
+  const theme = useTheme();
+
   return (
     <Stack
       screenOptions={{
-        headerShown: false, // <-- wyłączamy domyślny nagłówek
+        headerShown: false,
+        contentStyle: { backgroundColor: theme.colors.background },
+        presentation: "transparentModal",
+        animation: "default",
+        gestureEnabled: false,
       }}
-    />
+    >
+      <Stack.Screen
+        name="index"
+        options={{
+          contentStyle: { backgroundColor: theme.colors.background },
+        }}
+      />
+      <Stack.Screen
+        name="chooseVisitedCountries"
+        options={{
+          contentStyle: { backgroundColor: theme.colors.background },
+        }}
+      />
+    </Stack>
   );
 }
